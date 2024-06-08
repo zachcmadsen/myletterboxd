29a4c1ee4cba12170f6a971c7b1c76db1cd46e9e
Zach Madsen <zach@zachmadsen.com>
2024-06-07 20:37:10 -0700

    Set up axum and tokio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::Html, routing::get, Router};
+
+async fn hello() -> Html<&'static str> {
+    Html("<h1>hello, world</h1>")
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(hello));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
