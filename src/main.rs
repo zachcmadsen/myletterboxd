954868d343a648a287994863405aeccffbb0045e
Zach Madsen <zach@zachmadsen.com>
2024-06-08 18:09:14 -0700

    Add sqlx and example code that uses it

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use axum::{response::Html, routing::get, Router};
+use sqlx::SqlitePool;
 
 async fn hello() -> Html<&'static str> {
     Html("<h1>hello, world</h1>")
@@ -6,6 +7,28 @@ async fn hello() -> Html<&'static str> {
 
 #[tokio::main]
 async fn main() {
+    let pool = SqlitePool::connect("sqlite::memory:").await.unwrap();
+
+    sqlx::query(
+        r#"
+        CREATE TABLE todos (
+            id INTEGER PRIMARY KEY,
+            description TEXT NOT NULL
+        )
+        "#,
+    )
+    .execute(&pool)
+    .await
+    .unwrap();
+
+    let id = sqlx::query("INSERT INTO todos (description) VALUES (?1)")
+        .bind("cool description")
+        .execute(&pool)
+        .await
+        .unwrap()
+        .last_insert_rowid();
+    println!("row id: {}", id);
+
     let app = Router::new().route("/", get(hello));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
